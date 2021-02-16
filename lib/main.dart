d1708c2b625e3abb4ef3fddb0c0bb9d807412b20
Bea <beatriztattooer@gmail.com>
2021-02-16 20:45:15 -0300

    MAIN: added text and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,35 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+      'Are you an unicorn?'
+    ];
+
     return MaterialApp(
-      home:Text('Hello world! :)'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Trololo App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Option 01'),
+                onPressed: null
+            ),
+            RaisedButton(
+                child: Text('Option 02'),
+                onPressed: null
+            ),
+            RaisedButton(
+                child: Text('Option 03'),
+                onPressed: null
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
