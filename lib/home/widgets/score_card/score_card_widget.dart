844c532f4abb76a36ae12c87f821d48bcc4b6f2d
Bea <beatriztattooer@gmail.com>
2021-04-19 20:38:38 -0300

    CARD: padding on card

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -6,36 +6,39 @@ import 'package:flutter/material.dart';
 class ScoreCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 136,
-      decoration: BoxDecoration(
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(15)
-      ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children:[
-              Expanded(
-                flex:1,
-                child: ChartWidget()
-              ),
-              Expanded(
-                  flex:3,
-                  child:
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+          height: 136,
+          decoration: BoxDecoration(
+              color: AppColors.white,
+              borderRadius: BorderRadius.circular(15)
+          ),
+          child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children:[
+                Expanded(
+                    flex:1,
+                    child: ChartWidget()
+                ),
+                Expanded(
+                    flex:3,
+                    child:
                     Padding(
-                      padding: const EdgeInsets.only(left:24),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                       children: [
-                         Text("Let's start", style: AppTextStyles.heading,),
-                         Text("FInish the challenges and gain knowledge!", style: AppTextStyles.body,)
-                       ],
-                      )
+                        padding: const EdgeInsets.only(left:24),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Let's start", style: AppTextStyles.heading,),
+                            Text("FInish the challenges and gain knowledge!", style: AppTextStyles.body,)
+                          ],
+                        )
                     )
-              )
-            ]
-        )
+                )
+              ]
+          )
+      )
     );
   }
 }
