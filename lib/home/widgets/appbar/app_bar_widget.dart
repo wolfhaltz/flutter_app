3aea560ea81069fa4df4ca230bf1a2309f8f770d
Bea <beatriztattooer@gmail.com>
2021-04-19 19:47:36 -0300

    HOMEPAGE: img borderRadius

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -17,6 +17,7 @@ class AppBarWidget extends PreferredSize {
                     height: 58,
                     width: 58,
                     decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
                       image: DecorationImage(
                         image: NetworkImage(
                           "https://avatars.githubusercontent.com/u/37088290?v=4"
