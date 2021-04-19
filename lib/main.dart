15d63f25122f699491105e37e1fe6f2115c68095
Bea <beatriztattooer@gmail.com>
2021-04-19 18:20:19 -0300

    Cleanning

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,9 @@
 import 'package:flutter/material.dart';
+import 'core/app_widget.dart';
 
 // A versão mais extensa de escrever o main:
 // void main(){
-//   runApp(MyApp());
+//   runApp(AppWidget());
 // }
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // É aqui que coloco as funções que vão ser usadas:
-  void answerQuestion(){
-    print('Answer chosen!');
-  }
-
-  @override
-  Widget build(BuildContext context){
-    var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
-      'Are you an unicorn?'
-    ];
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Trololo App'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Text(
-                questions.elementAt(0),
-            ),
-            RaisedButton(
-              child: Text('Option 01'),
-                onPressed: ()=> { print('Aerial function!')},
-            ),
-            RaisedButton(
-                child: Text('Option 02'),
-                onPressed: answerQuestion
-            ),
-            RaisedButton(
-                child: Text('Option 03'),
-                onPressed: answerQuestion
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(AppWidget());
