bd879fdcc5aff4e7459a0df8b780639cb6a486ea
Bea <beatriztattooer@gmail.com>
2021-04-22 20:17:29 -0300

    QUEST INDICATOR: padding adjust

--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -7,18 +7,17 @@ class QuestionIndicatorWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Questão 04", style: AppTextStyles.body,),
-                Text("de 10", style: AppTextStyles.body,)
-              ],
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Questão 04", style: AppTextStyles.body,),
+              Text("de 10", style: AppTextStyles.body,)
+            ],
           ),
+          SizedBox(height: 16),
           ProgressIndicatorWidget(value: 0.7)
         ],
       ),
